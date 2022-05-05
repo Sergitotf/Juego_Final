53c091f575be0067aaba09670a48a3f4507f4806
Sergitotf <sergio_tf_vfx@hotmail.com>
2022-05-05 13:26:06 +0100

    05/05
    primera clase

---
+++ Juego Final/Assets/Walk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical"));
+    }
+}
